da9c548c141411b97651c80391905837134a3e9b
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2022-10-13 22:13:45 +0530

    Create matrix_zero.cpp

---
+++ matrix_zero.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void input(int matrix[][])
+{
+    int row, column;
+    string str;
+    cin >> str;
+    for(int i=0; i++; i<(str.length())-1)
+    {
+        if(str[i+2] == '\n')
+        {
+            return;
+        }
+        if()
+        matrix[row][column] = 
+    }
+}
+
+int main()
+{
+    int matrix [][];
+    void input(matrix);
+    
+    
+    cout<<row <<endl;
+    cout<<column;
+    
+    return 0;
+}
