004ea25ce5e5dcad020020639016430ca0fe6a3a
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2022-10-13 23:27:56 +0530

    working commit

--- matrix_zero.cpp
+++ matrix_zero.cpp
@@ -15,13 +15,14 @@ SIZE input(int matrix[100][100])
     cin >> str;
     for(int i=0; i<(str.length()-1); i++)
     {
-        if(str[i+2] == '\n')
+        if(str[i+2] == '\0')
         {
             break;
         }
         if(str[i]==']')
         {
             size.row++;
+            size.column =0;
             continue;
         }
         if(str[i]>47 && str[i]<58)
@@ -30,12 +31,41 @@ SIZE input(int matrix[100][100])
             size.column++;
         }
     }
+    size.row ++;
     return size;
 }
 
-void set_zero(SIZE size, int zero_matrix[size.row][size.column], int matrix[100][100])
+//void output(SIZE size, int zero_matrix[size.row][size.column])
+void output(SIZE size, int zero_matrix[100][100])
 {
-    std::copy(matrix[0][0], matrix[0][0] + size.row*size.column, zero_matrix[0][0]);
+    cout << "[";
+    for(int i=0; i<size.row; i++)
+    {
+        cout << "[";
+        for(int j=0; j<size.column; j++)
+        {
+            cout << zero_matrix[i][j] <<",";
+        }
+        cout << "\b]";
+    }
+    cout << "]";
+}
+
+void copy(SIZE size, int zero_matrix[100][100], int matrix[100][100])
+{
+    for(int i=0; i<size.row; i++)
+    {
+        for(int j=0; j<size.column; j++)
+        {
+            zero_matrix[i][j] = matrix[i][j];
+        }
+    }
+}
+
+//void set_zero(SIZE size, int zero_matrix[size.row][size.column], int matrix[100][100])
+void set_zero(SIZE size, int zero_matrix[100][100], int matrix[100][100])
+{
+    copy(size, zero_matrix, matrix);
     for(int i=0; i<size.row; i++)
     {
         for(int j=0; j<size.column; j++)
@@ -59,7 +89,8 @@ int main()
 {
     int matrix [100][100];
     SIZE size = input(matrix);
-    int zero_matrix[size.row][size.column];
+    int zero_matrix[100][100];
     set_zero(size, zero_matrix, matrix);
+    output(size, zero_matrix);
     return 0;
 }
